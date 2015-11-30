47142ba41a5ccd0aed2bf1f2dbc324e6cface383
Andrew Red <andrew@reddikh.com>
2015-11-30 16:03:18 +0300

    Initialized project with symfony 2.7 & npm & gulp

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # goose-site
 Just a goose website
+
+## Installation
+
+### Backend components
+```
+composer install
+```
+
+### Web components
+```
+npm i && gulp
+```
---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer');
+
+// sass
+gulp.task('default', function () {
+    return gulp.src('web/css/common.scss')
+        .pipe(sass())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions', '> 1%', 'ie 10'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('web/css/'));
+});
+
+gulp.task('copy', function () {
+    return gulp.src('node_modules/normalize.css/normalize.css')
+        .pipe(gulp.dest('web/css/'));
+});
